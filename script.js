a9f95adf98631df4f50aab685043af1b296f3511
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2020-10-30 15:50:35 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let pokemonList = [];
-pokemonList = [{ name: "bulbasaur", height:"7m" ,type: "grass"},{name: Marrill, height:"0.4m" ,type:"water"},{name:"gothorita" ,height:"0.7m" ,type:"Psychic"}];
+pokemonList = [{ name: "bulbasaur", height:"0.7m" ,[type: "grass"]}
+,{name: "Marrill", height:"0.4m" ,[type:"water"]},
+{name:"gothorita" ,height:"0.7m" ,[type:"Psychic"]}];
+for(let = 0; i<pokemonList.length i++){
+  if pokemonList[i.height<0.8 &&pokemonList[i].height>0.4]}
+  document.write(pokemonList[i].name + is " is a big one!");
+  else if (pokemonList[i].height<0.4){
+    document.write(pokemonList[i].name + is +" is a small one!");
+  }
+}
