41e52ea22b21b4e2693f88da005d93bc8f02be5b
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2020-10-31 15:38:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let pokemonList = [{ name: "bulbasaur", height:"0.7m" ,type: "grass"},
-{name: "Marrill", height:"0.4m" ,type:"water"},
-{name:"gothorita" ,height:"0.7m" ,type:"Psychic"}];
-
-for(let i= 0; i< pokemonList.length; i++){ if (height=== 0.7m ) { document.write("this is a big one") } else { document.write("this is a small one!")} }
-
+let pokemonList = [
+  { name: " Bulbasaur ", height:0.7 ,type: "Grass"},
+{name: " Marrill ", height:0.4 ,type:"Water"},
+{name:" Gothorita " ,height:0.7 ,type:"Psychic"}
+                  ];
+for(let i =0; i< pokemonList.length; i++){
+  if(pokemonList[i].height === 0.7){
+    document.write(pokemonList[i].name + " is a big pokemon!");
+  }else if (pokemonList[i].height == 0.4){
+    document.write(pokemonList[i].name +  "is tiny pokemon!");
+  } else {
+    document.write(pokemonList[i].name +" this pokemon is a different size");
   }
-}
