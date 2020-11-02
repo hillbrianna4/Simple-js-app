fc5e29a221fcee94f544849b01bbf21a2a8d75de
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2020-11-02 12:21:25 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let pokemonList = [
+
+var pokemonList = [
   { name: " Bulbasaur ", height:0.7 ,type: "Grass"},
 {name: " Marrill ", height:0.4 ,type:"Water"},
 {name:" Gothorita " ,height:0.7 ,type:"Psychic"}
                   ];
-for(let i =0; i< pokemonList.length; i++){
-  if(pokemonList[i].height === 0.7){
-    document.write(pokemonList[i].name + " is a big pokemon!");
-  }else if (pokemonList[i].height == 0.4){
-    document.write(pokemonList[i].name +  "is tiny pokemon!");
-  } else {
-    document.write(pokemonList[i].name +" this pokemon is a different size");
-  }
+function myLoopFunction(user) {
+  console.log(pokemonList.name + ' is ' + pokemonList.height + ' m tall');
+}
+pokemonList.forEach(myLoopFunction);
+
+function getAll(){
+  return pokemonList;
+}
+
+function add(pokemon){
+  pokemonList.push(pokemon);
+
+}
+
+return {
+  getAll: getAll,
+  add:add
+};
+})();
+
+console.log( pokemonList).getAll() );
