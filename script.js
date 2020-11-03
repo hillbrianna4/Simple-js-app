48509284882b22346d53c078cdb765ba12bc1cfb
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2020-11-03 11:33:13 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 
+
 var pokemonList = [
   { name: " Bulbasaur ", height:0.7 ,type: "Grass"},
 {name: " Marrill ", height:0.4 ,type:"Water"},
 {name:" Gothorita " ,height:0.7 ,type:"Psychic"}
                   ];
-function myLoopFunction(user) {
-  console.log(pokemonList.name + ' is ' + pokemonList.height + ' m tall');
-}
-pokemonList.forEach(myLoopFunction);
 
-function getAll(){
-  return pokemonList;
-}
-
-function add(pokemon){
-  pokemonList.push(pokemon);
-
-}
-
-return {
-  getAll: getAll,
-  add:add
-};
-})();
-
-console.log( pokemonList).getAll() );
+pokemonList.forEach(function(pokemonList){
+  document.write(pokemonList.name + ' is ' + pokemonList.height + ' m tall' + "<br>")
+});
