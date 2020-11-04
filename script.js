771e514a6d634be61c920fde4e31de6951bbc21e
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2020-11-04 09:07:03 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+
 var pokemonList = [
   { name: " Bulbasaur ", height:0.7 ,type: "Grass"},
 {name: " Marrill ", height:0.4 ,type:"Water"},
@@ -9,3 +10,21 @@ var pokemonList = [
 pokemonList.forEach(function(pokemonList){
   document.write(pokemonList.name + ' is ' + pokemonList.height + ' m tall' + "<br>")
 });
+
+!function(getAll){
+  return pokemonList.getAll();
+
+  !function add(pokemon) {
+    pokemonList.push(pokemon);
+
+  }
+
+
+
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+}();
+console.log( pokemonList.getAll());
